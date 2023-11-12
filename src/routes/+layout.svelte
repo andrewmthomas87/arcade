2a04d7d33810c29ad2e90fbef88018f4b6815f37
Andrew Thomas <andrewmthomas87@gmail.com>
2023-11-12 15:19:52 -0600

    Add palette, font, initial styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <link
+    href="https://api.fontshare.com/v2/css?f[]=general-sans@400,500,700&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>play</h1>
+<main>
+  <h1>arcade</h1>
+</main>
+
+<style>
+  h1 {
+    color: var(--blue600);
+    font-size: 3em;
+  }
+</style>
