150b8a7012f85bb881e7c8aaf890d613d4a3b0d8
Andrew Thomas <andrewmthomas87@gmail.com>
2023-11-12 20:53:58 -0600

    Add Apple II font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,4 @@
   import '../app.css';
 </script>
 
-<svelte:head>
-  <link
-    href="https://api.fontshare.com/v2/css?f[]=general-sans@400,500,700&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <slot />
