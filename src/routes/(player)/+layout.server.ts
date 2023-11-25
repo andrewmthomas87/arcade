36ccacda0096fc298600339831d8922e6554e84e
Andrew Thomas <andrewmthomas87@gmail.com>
2023-11-24 22:10:35 -0500

    Check for player in db

--- src/routes/(player)/+layout.server.ts
+++ src/routes/(player)/+layout.server.ts
@@ -1,10 +1,18 @@
 import { COOKIE, parsePlayerCookie } from '$lib/cookies';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import { db } from '$lib/server/db';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
   const player = parsePlayerCookie(cookies.get(COOKIE.player));
   if (!player) {
+    throw redirect(307, '/name');
+  }
+
+  const count = await db.player.count({ where: { id: player.id } });
+  if (count === 0) {
+    cookies.delete(COOKIE.player);
+
     throw redirect(307, '/name');
   }
 
