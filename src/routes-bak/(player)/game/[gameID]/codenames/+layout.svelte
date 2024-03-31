a70f2205228e64142337edaf0e44a9b2c31262b9
andrewmthomas87 <andrewmthomas87@gmail.com>
2024-03-31 01:09:25 -0600

    Remove routes-bak

--- src/routes-bak/(player)/game/[gameID]/codenames/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<slot />
-
-<style>
-  :global(body) {
-    background-image: url(/codenames-background-1bba6c4750aa55395eb19dfe45867096.png);
-  }
-</style>
--- src/routes-bak/(player)/game/[gameID]/doodledash/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<slot />
-
-<style>
-  :global(body) {
-    background-image: url(/doodledash-b120dfddaf87678acdb7c99d1952fd7f.png);
-  }
-</style>
